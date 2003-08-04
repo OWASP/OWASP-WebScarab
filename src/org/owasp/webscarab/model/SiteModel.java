25403f0ece782fd5f11b9a4ed41734c51f36ddfa
rogan <rogan>
2003-08-04 22:30:53 +0000

    reimplement the conversation table model

--- src/org/owasp/webscarab/model/SiteModel.java
+++ src/org/owasp/webscarab/model/SiteModel.java
@@ -23,19 +23,14 @@ import javax.swing.tree.DefaultMutableTreeNode;
 
 // SiteModel.java
 
-/** SiteModel represents the most significant part of the Exodus system. It contains the
+/** SiteModel represents the most significant part of the WebScarab system. It contains the
  * conversations, as well as the information about all the URLs that have been
- * seen. 
- * It is created as an Observable object, so that plugins can be notified of
- * any new Conversations, or URLInfos.
+ * seen.
  */
 public class SiteModel {
     
-    private Map conversation;         // records all the conversations seen.
-    private ArrayList conversationList;  // maintains a FIFO list of cached conversations
-    private ArrayList _conversationData = new ArrayList(1);  // Keeps the conversation data, is referenced by the ConversationTableModel
-    private Map urlinfo;              // maps urls to attrs
-    private ArrayList changed = new ArrayList(1);
+    private ArrayList _conversationList;  // maintains a FIFO list of cached conversations
+    private Map _urlinfo;              // maps urls to attrs
     private int _cachesize = 10;
     
     private Logger logger = Logger.getLogger("WebScarab.Model");
@@ -46,50 +41,29 @@ public class SiteModel {
      *  Constructor
      */
     public SiteModel() {
-        conversation = Collections.synchronizedMap(new TreeMap());
-        conversationList = new ArrayList(1);
-        urlinfo = Collections.synchronizedMap(new TreeMap());
+        _conversationList = new ArrayList(1);
+        _urlinfo = Collections.synchronizedMap(new TreeMap());
     } // constructor Model
     
     /** Initialises the lists of conversations, URLInfos, etc */
     public void clearSession() {
-        // clear the conversation cache
-        conversation.clear();
-        conversationList.clear();
+        // clear the conversation list
+        _conversationList.clear();
         // clear the conversationtablemodel
-        _conversationData.clear();
         _ctm.fireTableDataChanged();
-        // clear the URLInfo. Will go away when the site tree model is finished?
-        urlinfo.clear();
-    }
-    
-    private String currentConversationID = "00000";
-    
-    private synchronized String getNextConversationID() {
-        String id;
-        // increment the current id
-        currentConversationID = String.valueOf(Integer.parseInt(currentConversationID)+1);
-        for (int i=currentConversationID.length(); i<5; i++) {
-            currentConversationID = "0" + currentConversationID;
-        }
-        return currentConversationID;
-    }
-    
-    public void setConversationID(String id) {
-        if (Integer.parseInt(id) > Integer.parseInt(currentConversationID)) {
-            currentConversationID = id;
-        }
+        // clear the URLInfo.
+        _urlinfo.clear();
     }
     
     public String addConversation(Conversation conversation) {
         logger.info("Entering model's addConversation");
-        String id = getNextConversationID();
-        logger.info("Got an id");
-        cacheConversation(id, conversation);
+        _conversationList.add(conversation);
+        int row = _conversationList.size();
+        _ctm.fireTableRowsInserted(row, row);
         logger.info("Cached the conversation");
         parseResponse(conversation);
         logger.info("parsed the response");
-        return id;
+        return Integer.toString(_conversationList.size());
     }
     
     private void parseResponse(Conversation conversation) {
@@ -101,49 +75,33 @@ public class SiteModel {
         logger.info("parsed response");
     }
     
-    private void cacheConversation(String conversationID, Conversation c) {
-        if (!conversationList.contains(conversationID)) {
-            if (_cachesize > 0 && conversationList.size()>=_cachesize) {
-                String id = (String) conversationList.remove(0);
-                conversation.remove(id);
-            }
-            conversationList.add(conversationID);
-            conversation.put(conversationID, c);
-        } // else it is already cached
-    }
-    
     /** Given a conversation id, returns the corresponding Conversation
      * @return the requested Conversation, or null if it does not exist
      * @param id the requested "opaque" conversation id
      */
-    private Conversation getConversation(String id) {
-        synchronized (conversation) {
-            if (conversation.containsKey(id)) { // if we have it, return it
-                conversationList.remove(id); // move it to the end
-                conversationList.add(id);
-                return (Conversation)conversation.get(id);
-                
-            /* } else if (backingStore != null) { // if we can get it, cache it
-                Conversation c = backingStore.readConversation(id);
-                if (c!=null) {
-                    cacheConversation(id,c);
+    public Conversation getConversation(String id) {
+        try {
+            int pos = Integer.parseInt(id)-1;
+            synchronized (_conversationList) {
+                if (_conversationList.size()>pos) {
+                    return (Conversation) _conversationList.get(pos);
+                } else {
+                    throw new ArrayIndexOutOfBoundsException("ID " + id + " is out of bounds");
                 }
-                return c;
-             */
-            } else {  // sorry!
-                return null;
             }
+        } catch (NumberFormatException nfe) {
+            return null;
         }
     }
     
-    public URLInfo getURLInfo(Conversation conversation) {
+    public URLInfo createURLInfo(Conversation conversation) {
         String url = conversation.getProperty("URL");
         URLInfo ui;
-        synchronized (urlinfo) {
-            ui = (URLInfo) urlinfo.get(url);
+        synchronized (_urlinfo) {
+            ui = (URLInfo) _urlinfo.get(url);
             if (ui == null) {
                 ui = new URLInfo(url);
-                urlinfo.put(url, ui);
+                _urlinfo.put(url, ui);
             }
         }
         synchronized (ui) {
@@ -174,6 +132,13 @@ public class SiteModel {
         return ui;
     }
     
+    public URLInfo getURLInfo(String url) {
+        URLInfo ui;
+        synchronized (_urlinfo) {
+            ui = (URLInfo) _urlinfo.get(url);
+        }
+        return ui;
+    }
     
     public TableModel getConversationTableModel() {
         return _ctm;
@@ -181,23 +146,11 @@ public class SiteModel {
     
     public class ConversationTableModel extends AbstractTableModel {
         
-        public static final int ID = 0;
-        public static final int METHOD = 1;
-        public static final int SHPP = 2;
-        public static final int QUERY = 3;
-        public static final int COOKIE = 4;
-        public static final int CONTENT = 5;
-        public static final int STATUS = 6;
-        public static final int SETCOOKIE = 7;
-        public static final int CHECKSUM = 8;
-        public static final int COMMENT = 9;
-        public static final int ORIGIN = 10;
-        
         protected String [] columnNames = {
-            "ID", "Method", "Path", "Query",
-            "Cookies", "Content", "Status",
-            "Set-Cookie", "Checksum", "Comment",
-            "Origin"
+            "ID", "Method", "Url", "Query",
+            "Cookie", "Body", "Status",
+            "Set-Cookie", "Checksum", "Size",
+            "Origin", "Comment"
         };
         
         private Logger logger = Logger.getLogger("za.org.dragon.exodus.ConversationTableModel");
@@ -217,16 +170,20 @@ public class SiteModel {
         }
         
         public synchronized int getRowCount() {
-            return _conversationData.size();
+            return _conversationList.size();
         }
         
         public synchronized Object getValueAt(int row, int column) {
-            if (row<0 || row >= _conversationData.size()) {
+            if (row<0 || row >= _conversationList.size()) {
                 throw new ArrayIndexOutOfBoundsException("Attempt to get row " + row + ", column " + column + " : row does not exist!");
             }
-            String[] rowdata = (String[]) _conversationData.get(row);
+            Conversation c = (Conversation) _conversationList.get(row);
             if (column <= columnNames.length) {
-                return rowdata[column];
+                if (column == 0) {
+                    return new Integer(row+1).toString();
+                } else {
+                    return c.getProperty(columnNames[column].toUpperCase());
+                }
             } else {
                 throw new ArrayIndexOutOfBoundsException("Attempt to get row " + row + ", column " + column + " : column does not exist!");
             }
