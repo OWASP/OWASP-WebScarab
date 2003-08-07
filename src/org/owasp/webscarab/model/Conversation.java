0a28d831fcaf8cb8f6aa3df491dd5f08af301bf3
rogan <rogan>
2003-08-07 06:38:16 +0000

    Fixed incorrect method call

--- src/org/owasp/webscarab/model/Conversation.java
+++ src/org/owasp/webscarab/model/Conversation.java
@@ -36,7 +36,7 @@ public class Conversation {
         _props = new Properties();
         URL url = request.getURL();
         setProperty("METHOD", request.getMethod());
-        setProperty("URL", URLUtil.schemeAuthPath(url));
+        setProperty("URL", URLUtil.schemeHostPortPath(url));
         String value = url.getQuery();
         if (value != null) {
             setProperty("QUERY", value);
