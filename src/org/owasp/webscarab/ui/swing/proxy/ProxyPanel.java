54f27c92ddf51728fe52d85a7571109ebbd6f865
rogan <rogan>
2003-08-31 23:02:39 +0000

    provide an interface to the multiple listeners

--- src/org/owasp/webscarab/ui/swing/proxy/ProxyPanel.java
+++ src/org/owasp/webscarab/ui/swing/proxy/ProxyPanel.java
@@ -19,7 +19,8 @@ import java.util.ArrayList;
 public class ProxyPanel extends javax.swing.JPanel implements SwingPlugin {
     
     private Proxy _proxy;
-
+    private ListenerTableModel _ltm;
+    
     private ArrayList _plugins;
     private SwingPlugin[] _pluginArray = new SwingPlugin[0];
     
@@ -27,7 +28,9 @@ public class ProxyPanel extends javax.swing.JPanel implements SwingPlugin {
     public ProxyPanel(Proxy proxy) {
         _proxy = proxy;
         initComponents();
-        configure();
+        _ltm = new ListenerTableModel(proxy);
+        listenerTable.setModel(_ltm);
+        
         ManualEdit me = new ManualEdit();
         proxy.addPlugin(me);
         RevealHidden rh = new RevealHidden();
@@ -44,11 +47,6 @@ public class ProxyPanel extends javax.swing.JPanel implements SwingPlugin {
     public String getPluginName() {
         return new String("Proxy");
     }    
-    
-    private void configure() {
-        addressTextField.setText(_proxy.getListenServer());
-        portTextField.setText(Integer.toString(_proxy.getListenPort()));
-    }
     
     public void addPlugin(SwingPlugin plugin) {
         if (_plugins == null) {
@@ -68,12 +66,148 @@ public class ProxyPanel extends javax.swing.JPanel implements SwingPlugin {
         java.awt.GridBagConstraints gridBagConstraints;
 
         mainTabbedPane = new javax.swing.JTabbedPane();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        stopButton = new javax.swing.JButton();
+        startButton = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        listenerTable = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         addressTextField = new javax.swing.JTextField();
         portTextField = new javax.swing.JTextField();
+        baseTextField = new javax.swing.JTextField();
+        pluginsCheckBox = new javax.swing.JCheckBox();
 
         setLayout(new java.awt.GridBagLayout());
+
+        jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        jPanel2.setLayout(new java.awt.GridBagLayout());
+
+        stopButton.setText("Stop");
+        stopButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                stopButtonActionPerformed(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTH;
+        gridBagConstraints.weighty = 1.0;
+        jPanel2.add(stopButton, gridBagConstraints);
+
+        startButton.setText("Start");
+        startButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                startButtonActionPerformed(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
+        gridBagConstraints.weighty = 1.0;
+        jPanel2.add(startButton, gridBagConstraints);
+
+        listenerTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(listenerTable);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        jPanel2.add(jScrollPane1, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        jPanel1.add(jPanel2, gridBagConstraints);
+
+        jLabel1.setText("Address");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel1.add(jLabel1, gridBagConstraints);
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 0, 12));
+        jLabel2.setText("Port");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel1.add(jLabel2, gridBagConstraints);
+
+        jLabel3.setFont(new java.awt.Font("Dialog", 0, 12));
+        jLabel3.setText("Base URL");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel1.add(jLabel3, gridBagConstraints);
+
+        jLabel4.setFont(new java.awt.Font("Dialog", 0, 12));
+        jLabel4.setText("Use Plugins?");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel1.add(jLabel4, gridBagConstraints);
+
+        addressTextField.setToolTipText("Blank or \"*\" means all interfaces");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        jPanel1.add(addressTextField, gridBagConstraints);
+
+        portTextField.setToolTipText("integer between 0 and 65536");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        jPanel1.add(portTextField, gridBagConstraints);
+
+        baseTextField.setToolTipText("Blank for a conventional proxy, or http://host:port for a reverse proxy.");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        jPanel1.add(baseTextField, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel1.add(pluginsCheckBox, gridBagConstraints);
+
+        mainTabbedPane.addTab("Listeners", jPanel1);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
@@ -84,85 +218,50 @@ public class ProxyPanel extends javax.swing.JPanel implements SwingPlugin {
         gridBagConstraints.weighty = 1.0;
         add(mainTabbedPane, gridBagConstraints);
 
-        jLabel1.setText("Listen Address");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.insets = new java.awt.Insets(4, 4, 4, 4);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        add(jLabel1, gridBagConstraints);
-
-        jLabel2.setText("Port");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 2;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.insets = new java.awt.Insets(4, 4, 4, 4);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        add(jLabel2, gridBagConstraints);
-
-        addressTextField.setMinimumSize(new java.awt.Dimension(100, 19));
-        addressTextField.setPreferredSize(new java.awt.Dimension(125, 19));
-        addressTextField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                addressTextFieldActionPerformed(evt);
-            }
-        });
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.insets = new java.awt.Insets(4, 4, 4, 4);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        add(addressTextField, gridBagConstraints);
-
-        portTextField.setMinimumSize(new java.awt.Dimension(40, 19));
-        portTextField.setPreferredSize(new java.awt.Dimension(40, 19));
-        portTextField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                portTextFieldActionPerformed(evt);
-            }
-        });
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 3;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.insets = new java.awt.Insets(4, 4, 4, 4);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        gridBagConstraints.weightx = 1.0;
-        add(portTextField, gridBagConstraints);
-
     }//GEN-END:initComponents
 
-    private void portTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_portTextFieldActionPerformed
-        addrChanged();
-    }//GEN-LAST:event_portTextFieldActionPerformed
-
-    private void addressTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addressTextFieldActionPerformed
-        if (addressTextField.getText().equals("")) {
-            addressTextField.setText("*");
-        }
-        addrChanged();
-    }//GEN-LAST:event_addressTextFieldActionPerformed
-
-    private void addrChanged() {
+    private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startButtonActionPerformed
         try {
-            int port = Integer.parseInt(portTextField.getText());
-            if (port > 0 && port <65536) {
-                _proxy.setListenAddress(addressTextField.getText(), port);
-            } else {
-                throw new NumberFormatException("Number out of bounds!");
-            }
-        } catch (NumberFormatException nfe) {
-            portTextField.setText(Integer.toString(_proxy.getListenPort()));
+            _proxy.startProxy(addressTextField.getText(), Integer.parseInt(portTextField.getText()), baseTextField.getText(), pluginsCheckBox.isSelected());
+            addressTextField.setText("");
+            portTextField.setText("");
+            baseTextField.setText("");
+            _ltm.fireTableDataChanged();
+        } catch (Exception e) {
+            System.err.println("Error starting the listener : " + e);
         }
-    }
+    }//GEN-LAST:event_startButtonActionPerformed
+
+    private void stopButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_stopButtonActionPerformed
+        String[] keys = _proxy.getProxies();
+        int row = listenerTable.getSelectedRow();
+        if (row > -1 && row < keys.length) {
+            System.out.println("Key is '" + keys[row] + "'");
+            if (_proxy.stopProxy(keys[row])) {
+                System.out.println("Stopped " + keys[row]);
+            } else {
+                System.out.println("Failed to stop " + keys[row]);
+            }
+            _ltm.fireTableDataChanged();
+        }
+    }//GEN-LAST:event_stopButtonActionPerformed
         
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField addressTextField;
+    private javax.swing.JTextField baseTextField;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable listenerTable;
     private javax.swing.JTabbedPane mainTabbedPane;
+    private javax.swing.JCheckBox pluginsCheckBox;
     private javax.swing.JTextField portTextField;
+    private javax.swing.JButton startButton;
+    private javax.swing.JButton stopButton;
     // End of variables declaration//GEN-END:variables
     
 }
