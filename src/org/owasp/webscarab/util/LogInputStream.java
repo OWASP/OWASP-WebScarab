0e01bf286ba06e277186be3552e606d97a65d50c
rogan <rogan>
2004-04-19 08:08:19 +0000

    important close() on PrintStream when the InputStream is closed

--- src/org/owasp/webscarab/util/LogInputStream.java
+++ src/org/owasp/webscarab/util/LogInputStream.java
@@ -12,40 +12,97 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 /**
- *
- * @author  rdawes
+ * This class can be used to create a copy of the bytes that are read from the
+ * supplied InputStream
+ * @author rdawes
  */
 
 public class LogInputStream extends FilterInputStream {
-    InputStream _is;
-    PrintStream _ps;
+    private PrintStream _ps;
     
+    /**
+     * This class can be used to keep a copy of the bytes read from the supplied
+     * InputStream
+     * @param is The InputStream to read from
+     * @param ps the PrintStream to write any bytes read to. We use a PrintStream so that we don't have to worry about any Exceptions on the OutputStream
+     */    
     public LogInputStream(InputStream is, PrintStream ps) {
         super(is);
         if (is == null) {
             throw new NullPointerException("InputStream may not be null!");
         }
-        _is = is;
         _ps = ps;
     }
     
+    /**
+     * Reads the next byte of data from this input stream. The value
+     * byte is returned as an <code>int</code> in the range
+     * <code>0</code> to <code>255</code>. If no byte is available
+     * because the end of the stream has been reached, the value
+     * <code>-1</code> is returned. This method blocks until input data
+     * is available, the end of the stream is detected, or an exception
+     * is thrown.
+     * <p>
+     * This method
+     * simply performs <code>in.read()</code> and returns the result.
+     *
+     * @return     the next byte of data, or <code>-1</code> if the end of the
+     *             stream is reached.
+     * @exception  IOException  if an I/O error occurs.
+     * @see        java.io.FilterInputStream#in
+     */
     public int read() throws IOException {
         int b = super.read();
         if (b > -1) {
             _ps.write(b);
             _ps.flush();
+        } else {
+            // we close to signal downstream readers that the inputstream has closed
+            _ps.close();
         }
         return b;
     }
     
+    /**
+     * Reads up to <code>len</code> bytes of data from this input stream
+     * into an array of bytes. This method blocks until some input is
+     * available.
+     * <p>
+     * This method simply performs <code>in.read(b, off, len)</code>
+     * and returns the result.
+     *
+     * @param      b     the buffer into which the data is read.
+     * @param      off   the start offset of the data.
+     * @param      len   the maximum number of bytes read.
+     * @return     the total number of bytes read into the buffer, or
+     *             <code>-1</code> if there is no more data because the end of
+     *             the stream has been reached.
+     * @exception  IOException  if an I/O error occurs.
+     * @see        java.io.FilterInputStream#in
+     */
     public int read(byte[] b, int off, int len) throws IOException {
         int num = super.read(b, off, len);
         if (num > 0) {
             _ps.write(b,off,num);
             _ps.flush();
+        } else {
+            _ps.close();
         }
         return num;
     }
 
+    /**
+     * Tests if this input stream supports the <code>mark</code>
+     * and <code>reset</code> methods.
+     * This method always returns false, as we are not able to reset the
+     * PrintStream in an appropriate manner
+     * @return <code>false</code>
+     * @see java.io.FilterInputStream#in
+     * @see java.io.InputStream#mark(int)
+     * @see java.io.InputStream#reset()
+     */
+    public boolean markSupported() {
+        return false;
+    }
 }
 
