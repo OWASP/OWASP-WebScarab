d18eb6177fdac993aa865f4ce3044330b634076c
rogan <rogan>
2004-12-15 10:31:11 +0000

    Use Preferences instead of Properties, use new HTMLParser, bring analyser back into the notification thread.

--- src/org/owasp/webscarab/plugin/spider/Spider.java
+++ src/org/owasp/webscarab/plugin/spider/Spider.java
@@ -10,6 +10,7 @@ import org.owasp.webscarab.model.StoreException;
 import org.owasp.webscarab.model.HttpUrl;
 import org.owasp.webscarab.model.ConversationID;
 import org.owasp.webscarab.model.Cookie;
+import org.owasp.webscarab.model.Preferences;
 import org.owasp.webscarab.model.Request;
 import org.owasp.webscarab.model.Response;
 import org.owasp.webscarab.model.SiteModel;
@@ -24,13 +25,14 @@ import org.owasp.webscarab.httpclient.AsyncFetcher;
 
 import org.htmlparser.Node;
 
-import org.htmlparser.tags.LinkTag;
-import org.htmlparser.tags.CompositeTag;
-import org.htmlparser.tags.Tag;
+import org.htmlparser.Tag;
 
+import org.htmlparser.NodeFilter;
 import org.htmlparser.util.NodeIterator;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
+import org.htmlparser.filters.OrFilter;
+import org.htmlparser.filters.HasAttributeFilter;
 
 import java.util.Vector;
 import java.lang.ArrayIndexOutOfBoundsException;
@@ -64,8 +66,6 @@ public class Spider extends Plugin {
     private boolean _recursive = false;
     private boolean _cookieSync = true;
     
-    private Analyser _analyser;
-    
     private String _allowedDomains = null;
     private String _forbiddenPaths = null;
     
@@ -80,9 +80,7 @@ public class Spider extends Plugin {
     private String _status = "Stopped";
     
     /** Creates a new instance of Spider */
-    public Spider(Properties props) {
-        super(props);
-        
+    public Spider() {
         parseProperties();
     }
     
@@ -103,19 +101,19 @@ public class Spider extends Plugin {
     
     public void parseProperties() {
         String prop = "Spider.domains";
-        String value = _props.getProperty(prop, ".*localhost.*");
+        String value = Preferences.getPreference(prop, ".*localhost.*");
         setAllowedDomains(value);
         
         prop = "Spider.excludePaths";
-        value = _props.getProperty(prop, "");
+        value = Preferences.getPreference(prop, "");
         setForbiddenPaths(value);
         
         prop = "Spider.synchroniseCookies";
-        value = _props.getProperty(prop, "true");
+        value = Preferences.getPreference(prop, "true");
         setCookieSync(value.equalsIgnoreCase("true") || value.equalsIgnoreCase("yes"));
         
         prop = "Spider.recursive";
-        value = _props.getProperty(prop, "false");
+        value = Preferences.getPreference(prop, "false");
         setRecursive(value.equalsIgnoreCase("true") || value.equalsIgnoreCase("yes"));
     }
     
@@ -128,19 +126,11 @@ public class Spider extends Plugin {
         _stopping = false;
         _runThread = Thread.currentThread();
         
-        // start the analyser thread
-        _analyser = new Analyser();
-        Thread t = new Thread(_analyser);
-        t.setDaemon(true);
-        t.setName("Spider-Analyser");
-        t.setPriority(Thread.MIN_PRIORITY);
-        t.start();
-        
         // start the fetchers
         _fetchers = new AsyncFetcher[_threads];
         for (int i=0; i<_threads; i++) {
             _fetchers[i] = new AsyncFetcher(_requestQueue, _responseQueue);
-            t = new Thread(_fetchers[i], "Spider-" + Integer.toString(i));
+            Thread t = new Thread(_fetchers[i], "Spider-" + Integer.toString(i));
             t.setDaemon(true);
             t.setPriority(Thread.MIN_PRIORITY);
             t.start();
@@ -154,12 +144,13 @@ public class Spider extends Plugin {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException ie) {}
+            } else {
+                Thread.yield();
             }
         }
         for (int i=0; i<_fetchers.length; i++) {
             _fetchers[i].stop();
         }
-        _analyser.stop();
         _running = false;
         _runThread = null;
         if (_ui != null) _ui.setEnabled(_running);
@@ -173,8 +164,7 @@ public class Spider extends Plugin {
         synchronized (_linkQueue) {
             if (_linkQueue.size() > 0 && _requestQueue.size() == 0) {
                 link = (Link) _linkQueue.remove(0);
-                _logger.info(_linkQueue.size() + " remaining, queueing " + link.getURL());
-                if (_ui != null) _ui.linkDequeued(link);
+                if (_ui != null) _ui.linkDequeued(link, _linkQueue.size());
             } else {
                 return false;
             }
@@ -236,7 +226,7 @@ public class Spider extends Plugin {
     public boolean isBusy() {
         if (!_running) return false;
         synchronized(_linkQueue) {
-            return _linkQueue.size() > 0 || _analyser.isBusy();
+            return _linkQueue.size() > 0;
         }
     }
     
@@ -297,10 +287,12 @@ public class Spider extends Plugin {
     
     private void queueLink(Link link) {
         _logger.info("Queueing " + link);
+        int size;
         synchronized (_linkQueue) {
             _linkQueue.add(link);
+            size = _linkQueue.size();
         }
-        if (_ui != null) _ui.linkQueued(link);
+        if (_ui != null) _ui.linkQueued(link, size);
     }
     
     /** removes all pending requests from the queues - effectively stops the spider */
@@ -309,18 +301,18 @@ public class Spider extends Plugin {
         synchronized(_linkQueue) {
             while(_linkQueue.size()>0) {
                 link = (Link) _linkQueue.remove(0);
-                if (_ui != null) _ui.linkDequeued(link);
             }
         }
         synchronized(_requestQueue) {
             _requestQueue.clear();
         }
+        if (_ui != null) _ui.linkDequeued(null, 0);
     }
     
     public void setRecursive(boolean bool) {
         _recursive = bool;
         String prop = "Spider.recursive";
-        _props.setProperty(prop,Boolean.toString(bool));
+        Preferences.setPreference(prop,Boolean.toString(bool));
     }
     
     public boolean getRecursive() {
@@ -330,7 +322,7 @@ public class Spider extends Plugin {
     public void setCookieSync(boolean enabled) {
         _cookieSync = enabled;
         String prop = "Spider.synchroniseCookies";
-        _props.setProperty(prop,Boolean.toString(enabled));
+        Preferences.setPreference(prop,Boolean.toString(enabled));
     }
     
     public boolean getCookieSync() {
@@ -383,7 +375,7 @@ public class Spider extends Plugin {
     public void setAllowedDomains(String regex) {
         _allowedDomains = regex;
         String prop = "Spider.domains";
-        _props.setProperty(prop,regex);
+        Preferences.setPreference(prop,regex);
     }
     
     public String getAllowedDomains() {
@@ -393,7 +385,7 @@ public class Spider extends Plugin {
     public void setForbiddenPaths(String regex) {
         _forbiddenPaths = regex;
         String prop = "Spider.excludePaths";
-        _props.setProperty(prop,regex);
+        Preferences.setPreference(prop,regex);
     }
     
     public String getForbiddenPaths() {
@@ -416,17 +408,142 @@ public class Spider extends Plugin {
     }
     
     public String getStatus() {
-        if (isBusy()) 
-            return "Started, " + 
-            _analyser.queueSize() + " queued for analysis, " + 
+        if (isBusy())
+            return "Started, " +
             _linkQueue.size() + " queued for fetching";
         return _status;
+    }
+    
+    private void analyse(ConversationID id) {
+        Request request = _model.getRequest(id);
+        Response response = _model.getResponse(id);
+        HttpUrl base = request.getURL();
+        if (response.getStatus().equals("302")) {
+            String location = response.getHeader("Location");
+            if (location != null) {
+                try {
+                    HttpUrl url = new HttpUrl(location);
+                    addUnseenLink(url, base);
+                } catch (MalformedURLException mue) {
+                    _logger.warning("Badly formed Location header : " + location);
+                }
+            } else {
+                _logger.warning("302 received, but no Location header!");
+            }
+            return;
+        }
+        Object parsed = Parser.parse(base, response);
+        if (parsed != null && parsed instanceof NodeList) { // the parsed content is HTML
+            NodeList nodelist = (NodeList) parsed;
+            processHtml(base, nodelist);
+            // recurseHtmlNodes(nodelist, base);
+        } // else maybe it is a parsed Flash document? Anyone? :-)
+    }
+    
+    /*private */void processHtml(HttpUrl base, NodeList nodelist) {
+        NodeFilter filter = new HasAttributeFilter("href");
+        filter = new OrFilter(filter, new HasAttributeFilter("src"));
+        filter = new OrFilter(filter, new HasAttributeFilter("onclick"));
+        filter = new OrFilter(filter, new HasAttributeFilter("onblur"));
+        try {
+            NodeList links = nodelist.extractAllNodesThatMatch(filter);
+            for (NodeIterator ni = links.elements(); ni.hasMoreNodes(); ) {
+                Node node = ni.nextNode();
+                if (node instanceof Tag) {
+                    boolean got = false;
+                    Tag tag = (Tag) node;
+                    String src = tag.getAttribute("src");
+                    if (src != null) {
+                        processLink(base, src);
+                        got = true;
+                    }
+                    String href = tag.getAttribute("href");
+                    if (href != null) {
+                        processLink(base, href);
+                        got = true;
+                    }
+                    if (!got) {
+                        _logger.info("Didn't get anything from " + tag.getClass().getName() + ": " + tag);
+                    }
+                }
+            }
+        } catch (ParserException pe) {
+            _logger.warning("ParserException : " + pe);
+        }
+    }
+    
+    /*
+    private void recurseHtmlNodes(HttpUrl base, NodeList nodelist) {
+        try {
+            for (NodeIterator ni = nodelist.elements(); ni.hasMoreNodes();) {
+                Node node = ni.nextNode();
+                if (node instanceof LinkTag) {
+                    LinkTag linkTag = (LinkTag) node;
+                    processLink(base, linkTag.getLink());
+                } else if (node instanceof CompositeTag) {
+                    CompositeTag ctag = (CompositeTag) node;
+                    recurseHtmlNodes(ctag.getChildren(), base);
+                } else if (node instanceof Tag) { // this is horrendous! Why is this not a FrameTag?!
+                    Tag tag = (Tag) node;
+                    if (tag.getTagName().equals("FRAME")) {
+                        processLink(base, tag.getAttribute("src"));
+                    }
+                }
+            }
+        } catch (ParserException pe) {
+            _logger.warning("ParserException : " + pe);
+        }
+    }
+     */
+    private void processLink(HttpUrl base, String link) {
+        if (link.startsWith("http://") || link.startsWith("https://")) {
+            try {
+                HttpUrl url = new HttpUrl(link);
+                addUnseenLink(url, base);
+            } catch (MalformedURLException mue) {
+                _logger.warning("Malformed link : " + link);
+            }
+        } else if (link.startsWith("mailto:")) {
+            // do nothing
+        } else if (link.startsWith("javascript:")) {
+            processScript(base, link.substring(10));
+        } else if (link.matches("[a-zA-Z]+://.*")) {
+            _logger.info("Encountered an unhandled url scheme " + link);
+        } else {
+            _logger.fine("Creating a new relative URL with " + base + " and " + link + " '");
+            try {
+                HttpUrl url = new HttpUrl(base, link);
+                addUnseenLink(url, base);
+            } catch (MalformedURLException mue) {
+                _logger.warning("Bad relative URL (" + base.toString() + ") : " + link);
+            }
+        }
+    }
+    
+    private void processScript(HttpUrl base, String script) {
+        if (script.startsWith("window.open(")) {
+            _logger.info("Script opens a window : " + script);
+        } else if (script.startsWith("location.href")) {
+            _logger.info("Script sets location : " + script);
+        }
+    }
+    
+    private void addUnseenLink(HttpUrl url, HttpUrl referer) {
+        if (url == null) {
+            return;
+        }
+        if (_model.getConversationCount(url) == 0) {
+            String first = _model.getUrlProperty(url, "REFERER");
+            if (first == null || first.equals("")) {
+                _model.setUrlProperty(url, "REFERER", referer.toString());
+            }
+        }
     }
     
     private class Listener extends SiteModelAdapter {
         
         public void conversationAdded(ConversationID id) {
-            _analyser.queue(id);
+            analyse(id);
         }
         
         public void urlAdded(HttpUrl url) {
@@ -436,147 +553,4 @@ public class Spider extends Plugin {
         
     }
     
-    private class Analyser implements Runnable {
-        
-        private Vector _queue = new Vector(); // is synchronized already
-        private boolean _stopping = false;
-        private boolean _running = false;
-        private Thread _runThread = null;
-        
-        public void queue(ConversationID id) {
-            _queue.add(id);
-        }
-        
-        private int queueSize() {
-            return _queue.size();
-        }
-        
-        public boolean isBusy() {
-            return _queue.size() > 0;
-        }
-        
-        public void reset() {
-            _queue.clear();
-        }
-        
-        public void run() {
-            _runThread = Thread.currentThread();
-            _stopping = false;
-            _running = true;
-            while (!_stopping) {
-                if (_queue.size()>0) {
-                    ConversationID id = (ConversationID) _queue.remove(0);
-                    _logger.info("Analysing " + id);
-                    analyse(id);
-                } else {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException ie) {}
-                }
-            }
-            _running = false;
-            _runThread = null;
-        }
-        
-        public boolean stop() {
-            if (_running) {
-                _stopping = true;
-                try {
-                    _runThread.join(5000);
-                } catch (InterruptedException ie) {
-                    _logger.warning("Interrupted!");
-                }
-            }
-            return ! _running;
-        }
-        
-        private void analyse(ConversationID id) {
-            Request request = _model.getRequest(id);
-            Response response = _model.getResponse(id);
-            HttpUrl base = request.getURL();
-            if (response.getStatus().equals("302")) {
-                String location = response.getHeader("Location");
-                if (location != null) {
-                    try {
-                        HttpUrl url = new HttpUrl(location);
-                        addUnseenLink(url, base);
-                    } catch (MalformedURLException mue) {
-                        _logger.warning("Badly formed Location header : " + location);
-                    }
-                } else {
-                    _logger.warning("302 received, but no Location header!");
-                }
-                return;
-            }
-            Object parsed = Parser.parse(base, response);
-            if (parsed != null && parsed instanceof NodeList) { // the parsed content is HTML
-                NodeList nodelist = (NodeList) parsed;
-                recurseHtmlNodes(nodelist, base);
-            } // else maybe it is a parsed Flash document? Anyone? :-)
-        }
-        
-        private void recurseHtmlNodes(NodeList nodelist, HttpUrl base) {
-            try {
-                for (NodeIterator ni = nodelist.elements(); ni.hasMoreNodes();) {
-                    Node node = ni.nextNode();
-                    if (node instanceof LinkTag) {
-                        LinkTag linkTag = (LinkTag) node;
-                        if (! linkTag.isHTTPLikeLink() )
-                            continue;
-                        String link = linkTag.getLink();
-                        // for some reason the htmlparser thinks IRC:// links are httpLike
-                        if (link == null || link.startsWith("irc://"))
-                            continue;
-                        try {
-                            HttpUrl url = new HttpUrl(link);
-                            addUnseenLink(url, base);
-                        } catch (MalformedURLException mue) {
-                            // FIXME: We should also do SOMETHING with javascript links, maybe just show them
-                            // and provide a link to where they came from?
-                            _logger.warning("Malformed link: '" + link + "'");
-                        }
-                    } else if (node instanceof CompositeTag) {
-                        CompositeTag ctag = (CompositeTag) node;
-                        recurseHtmlNodes(ctag.getChildren(), base);
-                    } else if (node instanceof Tag) { // this is horrendous! Why is this not a FrameTag?!
-                        Tag tag = (Tag) node;
-                        if (tag.getTagName().equals("FRAME")) {
-                            String src = tag.getAttribute("src");
-                            if (src.startsWith("http://") || src.startsWith("https://")) {
-                                try {
-                                    HttpUrl url = new HttpUrl(src);
-                                    addUnseenLink(url, base);
-                                } catch (MalformedURLException mue) {
-                                    _logger.warning("Malformed Frame src : " + src);
-                                }
-                            } else if (!src.startsWith("about:")) {
-                                _logger.fine("Creating a new relative URL with " + base + " and " + src + " '");
-                                try {
-                                    HttpUrl url = new HttpUrl(base, src);
-                                    addUnseenLink(url, base);
-                                } catch (MalformedURLException mue) {
-                                    _logger.warning("Bad relative URL (" + base.toString() + ") : " + src);
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch (ParserException pe) {
-                _logger.warning("ParserException : " + pe);
-            }
-        }
-        
-        private void addUnseenLink(HttpUrl url, HttpUrl referer) {
-            if (url == null) {
-                return;
-            }
-            if (_model.getConversationCount(url) == 0) {
-                String first = _model.getUrlProperty(url, "REFERER");
-                if (first == null || first.equals("")) {
-                    _model.setUrlProperty(url, "REFERER", referer.toString());
-                }
-            }
-        }
-        
-    }
 }
