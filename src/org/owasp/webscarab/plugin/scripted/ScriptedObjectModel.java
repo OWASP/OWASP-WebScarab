9be8c0ad2217351d42f63c8f1072c6d109a564ed
rogan <rogan>
2005-03-11 16:38:36 +0000

    Make more of the sitemodel methods available in the script

--- src/org/owasp/webscarab/plugin/scripted/ScriptedObjectModel.java
+++ src/org/owasp/webscarab/plugin/scripted/ScriptedObjectModel.java
@@ -15,10 +15,12 @@ import org.owasp.webscarab.model.HttpUrl;
 import org.owasp.webscarab.plugin.Framework;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 /**
- *
- * @author  rogan
+ * Provides a wrapper around the Scripting plugin, so that we do not expose
+ * potentially dangerous methods, such as adding listeners, stopping the plugin, etc
+ * @author rogan
  */
 public class ScriptedObjectModel {
     
@@ -26,23 +28,104 @@ public class ScriptedObjectModel {
     private SiteModel _model;
     private Scripted _scripted;
     
-    /** Creates a new instance of ScriptedObjectModel */
+    /**
+     * Creates a new instance of ScriptedObjectModel
+     * @param framework The framework that holds WebScarab together
+     * @param scripted the scripting plugin that we interact with
+     */
     public ScriptedObjectModel(Framework framework, Scripted scripted) {
         _framework = framework;
         _model = _framework.getModel();
         _scripted = scripted;
     }
     
+    /**
+     * adds a conversation to the overall WebScarab framework.
+     * All such conversations will be processed by all of the other plugins, and will be
+     * available for later review.
+     * The Response includes the corresponding Request as a parameter.
+     * @param response the Response to add to the framework
+     * @return the ConversationID allocated to the conversation
+     */    
     public ConversationID addConversation(Response response) {
         return _framework.addConversation(response.getRequest(), response, "Scripted");
     }
     
+    /**
+     * convenience method, saving the user from doing "new ConversationID(1)" each time
+     * returns a copy of the desired Request, or null if the id did not exist
+     * @param id the numerical id of the request
+     * @return a copy of the desired Request, or null if it did not exist
+     */    
     public Request getRequest(int id) {
-        Request request = _model.getRequest(new ConversationID(id));
+        return getRequest(new ConversationID(id));
+    }
+    
+    public Request getRequest(ConversationID id) {
+        Request request = _model.getRequest(id);
         if (request == null) return request;
         return new Request(request);
     }
     
+    public Response getResponse(int id) {
+        return getResponse(new ConversationID(id));
+    }
+    
+    public Response getResponse(ConversationID id) {
+        Response response = _model.getResponse(id);
+        if (response == null) return response;
+        return new Response(response);
+    }
+    
+    public int getChildUrlCount(String url) throws MalformedURLException {
+        HttpUrl myUrl = null;
+        if (url != null) myUrl = new HttpUrl(url);
+        return _model.getChildUrlCount(myUrl);
+    }
+    
+    public HttpUrl getChildUrlAt(String url, int index) throws MalformedURLException {
+        HttpUrl myUrl = null;
+        if (url != null) myUrl = new HttpUrl(url);
+        return _model.getChildUrlAt(myUrl, index);
+    }
+    
+    public int getQueryCount(String url) throws MalformedURLException {
+        HttpUrl myUrl = null;
+        if (url != null) myUrl = new HttpUrl(url);
+        return _model.getQueryCount(myUrl);
+    }
+    
+    public HttpUrl getQueryAt(String url, int index) throws MalformedURLException {
+        HttpUrl myUrl = null;
+        if (url != null) myUrl = new HttpUrl(url);
+        return _model.getQueryAt(myUrl, index);
+    }
+    
+    public String getUrlProperty(String url, String property) throws MalformedURLException {
+        HttpUrl myUrl = null;
+        if (url != null) myUrl = new HttpUrl(url);
+        return _model.getUrlProperty(myUrl, property);
+    }
+    
+    public int getConversationCount(String url) throws MalformedURLException {
+        HttpUrl myUrl = null;
+        if (url != null) myUrl = new HttpUrl(url);
+        return _model.getConversationCount(myUrl);
+    }
+    
+    public ConversationID getConversationAt(String url, int index) throws MalformedURLException {
+        HttpUrl myUrl = null;
+        if (url != null) myUrl = new HttpUrl(url);
+        return _model.getConversationAt(myUrl, index);
+    }
+    
+    public String getConversationProperty(int id, String property) {
+        return getConversationProperty(new ConversationID(id), property);
+    }
+    
+    public String getConversationProperty(ConversationID id, String property) {
+        return _model.getConversationProperty(id, property);
+    }
     public Response fetchResponse(Request request) throws IOException {
         return _scripted.fetchResponse(request);
     }
