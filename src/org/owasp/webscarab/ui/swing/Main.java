5c4d70d6836242be61066cb3411f47cbd6da5383
rogan <rogan>
2005-04-05 11:37:13 +0000

    Re-arrange plugin load order

--- src/org/owasp/webscarab/ui/swing/Main.java
+++ src/org/owasp/webscarab/ui/swing/Main.java
@@ -184,17 +184,6 @@ public class Main {
     }
     
     public static void loadPlugins() {
-        Fragments fragments = new Fragments(_framework);
-        _framework.addPlugin(fragments);
-        _uif.addPlugin(new FragmentsPanel(fragments));
-        
-        /* Not ready for prime time yet! */
-        Fuzzer fuzzer = new Fuzzer(_framework);
-        _framework.addPlugin(fuzzer);
-        FuzzerPanel fuzzerPanel = new FuzzerPanel(fuzzer);
-        _uif.addPlugin(fuzzerPanel);
-        /* */
-        
         Proxy proxy = new Proxy(_framework);
         _framework.addPlugin(proxy);
         ProxyPanel proxyPanel = new ProxyPanel(proxy);
@@ -202,13 +191,13 @@ public class Main {
         
         loadProxyPlugins(proxy, proxyPanel);
         
-        Spider spider = new Spider(_framework);
-        _framework.addPlugin(spider);
-        _uif.addPlugin(new SpiderPanel(spider));
-        
         ManualRequest manualRequest = new ManualRequest(_framework);
         _framework.addPlugin(manualRequest);
         _uif.addPlugin(new ManualRequestPanel(manualRequest));
+        
+        Spider spider = new Spider(_framework);
+        _framework.addPlugin(spider);
+        _uif.addPlugin(new SpiderPanel(spider));
         
         SessionIDAnalysis sessionIDAnalysis = new SessionIDAnalysis(_framework);
         _framework.addPlugin(sessionIDAnalysis);
@@ -217,6 +206,18 @@ public class Main {
         Scripted scripted = new Scripted(_framework);
         _framework.addPlugin(scripted);
         _uif.addPlugin(new ScriptedPanel(scripted));
+        
+        Fragments fragments = new Fragments(_framework);
+        _framework.addPlugin(fragments);
+        _uif.addPlugin(new FragmentsPanel(fragments));
+        
+        /* Not ready for prime time yet!
+        Fuzzer fuzzer = new Fuzzer(_framework);
+        _framework.addPlugin(fuzzer);
+        FuzzerPanel fuzzerPanel = new FuzzerPanel(fuzzer);
+        _uif.addPlugin(fuzzerPanel);
+        /* */
+        
     }
     
     public static void loadProxyPlugins(Proxy proxy, ProxyPanel proxyPanel) {
