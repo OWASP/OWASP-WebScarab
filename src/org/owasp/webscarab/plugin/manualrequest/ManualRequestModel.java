3ef28038463fbcdd202880ab1ad32a02061dbbb0
rogan <rogan>
2005-05-18 15:14:39 +0000

    Implement a model for the ManualRequest plugin

---
+++ src/org/owasp/webscarab/plugin/manualrequest/ManualRequestModel.java
@@ -0,0 +1,42 @@
+/*
+ * ManualRequestModel.java
+ *
+ * Created on 01 May 2005, 11:12
+ */
+
+package org.owasp.webscarab.plugin.manualrequest;
+
+import org.owasp.webscarab.model.FrameworkModel;
+import org.owasp.webscarab.model.ConversationModel;
+import org.owasp.webscarab.model.Cookie;
+import org.owasp.webscarab.model.HttpUrl;
+
+import org.owasp.webscarab.plugin.AbstractPluginModel;
+
+/**
+ *
+ * @author  rogan
+ */
+public class ManualRequestModel extends AbstractPluginModel {
+    
+    private FrameworkModel _model;
+    
+    /** Creates a new instance of ManualRequestModel */
+    public ManualRequestModel(FrameworkModel model) {
+        super(model);
+        _model = model;
+    }
+    
+    public ConversationModel getConversationModel() {
+        return _model.getConversationModel();
+    }
+    
+    public Cookie[] getCookiesForUrl(HttpUrl url) {
+        return _model.getCookiesForUrl(url);
+    }
+    
+    public void addCookie(Cookie cookie) {
+        _model.addCookie(cookie);
+    }
+    
+}
