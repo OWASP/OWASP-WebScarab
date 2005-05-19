4ea6aa1166f16e742c47950cd004b8b3c30dfdb7
rogan <rogan>
2005-05-19 15:24:13 +0000

    bugfixes

--- src/org/owasp/webscarab/util/LevenshteinDistance.java
+++ src/org/owasp/webscarab/util/LevenshteinDistance.java
@@ -1,16 +1,35 @@
 package org.owasp.webscarab.util;
 
+
+/*
+ * Calculates the Levenshtein distance between two byte arrays
+ * This is great for showing which responses are similar or different
+ * to others. However, it is VERY slow, O(n*m), which bogs down really
+ * quickly if we start looking at sequences of a few thousand bytes :-(
+ *
+ * An alternative might be the XDelta algorithm, see e.g. 
+ * http://sourceforge.net/projects/javaxdelta/&e=10313
+ * 
+ * Also see a paper "A Linear Time, Constant Space Differencing Algorithm" by Burns and Long
+ */
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 public class LevenshteinDistance {
     
     private byte[] _baseline;
+    private int[] _current, _previous;
+    
+    private Logger _logger = Logger.getLogger(getClass().getName());
     
     public LevenshteinDistance(byte[] baseline) {
         _baseline = baseline;
+        _current = new int[_baseline.length+1];
+        _previous = new int[_baseline.length+1];
     }
     
-    public int getDistance(byte[] target) {
-        int[] current = new int[_baseline.length+1];
-        int[] previous = new int[_baseline.length+1];
+    public synchronized int getDistance(byte[] target) {
         int i,j,cost;
         
         if (_baseline.length == 0)
@@ -19,15 +38,17 @@ public class LevenshteinDistance {
             return _baseline.length;
         
         for (i = 0; i <= _baseline.length; i++) {
-            current[i] = i;
+            _current[i] = i;
         }
         
+        _logger.info("Comparing " + _baseline.length + " and " + target.length);
+        
         for (i = 1; i <= _baseline.length; i++) {
-            int[] t = previous;
-            previous = current;
-            current = t;
+            int[] t = _previous;
+            _previous = _current;
+            _current = t;
             
-            current[0] = previous[0]+1;
+            _current[0] = _previous[0]+1;
             
             for (j = 1; j <= target.length; j++) {
                 if (_baseline[i-1] == target[j-1]) {
@@ -35,10 +56,11 @@ public class LevenshteinDistance {
                 } else {
                   cost = 1;
                 }
-                current[j] = Math.min(Math.min(previous[j]+1, current[j-1]+1), previous[j-1] + cost);
+                _current[i] = Math.min(Math.min(_previous[i]+1, _current[i-1]+1), _previous[i-1] + cost);
             }
         }
-        return current[_baseline.length];
+        _logger.info("done");
+        return _current[_baseline.length];
     }
 
     public static void main(String[] args) {
