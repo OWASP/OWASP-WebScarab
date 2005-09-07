3a5992e9362c4ae2bdb3d28067633706e2ce4a98
rogan <rogan>
2005-09-07 15:40:11 +0000

    enhancements, not enabled yet

--- src/org/owasp/webscarab/plugin/SSLKeyManager.java
+++ src/org/owasp/webscarab/plugin/SSLKeyManager.java
@@ -17,6 +17,8 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.Principal;
 import java.security.PrivateKey;
+import java.security.Provider;
+import java.security.Security;
 import java.security.cert.X509Certificate;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -33,15 +35,25 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import java.io.IOException;
 import java.io.FileInputStream;
+
+import java.beans.PropertyChangeSupport;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
+import org.owasp.webscarab.model.Preferences;
 
 /**
  *
  * @author rdawes
  */
 public class SSLKeyManager implements X509KeyManager {
+    
+    public final static String KEY_PROPERTY = "KEYS";
+    public final static String SELECTED_KEY = "SELECTED KEY";
     
     private static final String SEP = " -:- ";
     
@@ -52,20 +64,43 @@ public class SSLKeyManager implements X509KeyManager {
     private Map _stores = new TreeMap();
     private Map _managers = new TreeMap();
     
+    private PropertyChangeSupport _changeSupport = new PropertyChangeSupport(this);
+    
     private Logger _logger = Logger.getLogger(getClass().getName());
     
     /**
-     * Creates a new instance of SSLKeyManager 
+     * Creates a new instance of SSLKeyManager
      */
     public SSLKeyManager() {
+        _logger.setLevel(Level.FINEST);
+        if (System.getProperty("os.name", "").toLowerCase().indexOf("windows")>-1) {
+            Provider provider;
+            try {
+                provider = (Provider) Class.forName("se.assembla.jce.provider.ms.MSProvider").newInstance();
+            } catch (Throwable t) {
+                return;
+            }
+            try {
+                Security.insertProviderAt(provider, 2);
+                KeyStore ks = KeyStore.getInstance("msks", "assembla");
+                ks.load(null, null);
+                addKeyStore("Microsoft CAPI store", ks, null);
+            } catch (Exception e) {
+                _logger.info("Microsoft CAPI interface not available: " + e);
+            }
+        }
     }
     
-    public synchronized String addPKCS12KeyStore(String filename, char[] keyStorePassword, char[] keyPassword) throws KeyStoreException, UnrecoverableKeyException, IOException, CertificateException {
+    public synchronized String addPKCS12KeyStore(String filename, String keyStorePassword, String keyPassword) throws KeyStoreException, UnrecoverableKeyException, IOException, CertificateException {
+        if (keyStorePassword == null) keyStorePassword = "";
+        if (keyPassword == null) keyPassword = keyStorePassword;
+        
         try {
             KeyStore ks = KeyStore.getInstance("PKCS12");
-            ks.load(new FileInputStream(filename), keyStorePassword);
+            ks.load(new FileInputStream(filename), keyStorePassword.toCharArray());
             String description = "PKCS#12: " + filename;
-            addKeyStore(description, ks, keyPassword);
+            addKeyStore(description, ks, keyPassword.toCharArray());
+            
             return description;
         } catch (NoSuchAlgorithmException nsae) {
             _logger.severe("No SunX509 suport: " + nsae);
@@ -85,6 +120,7 @@ public class SSLKeyManager implements X509KeyManager {
         } catch (NoSuchAlgorithmException nsae) {
             _logger.severe("This should never happen! SunX509 algorithm not found: " + nsae.getMessage());
         }
+        _changeSupport.firePropertyChange(KEY_PROPERTY, null, null);
     }
     
     public String[] getKeyStoreDescriptions() {
@@ -93,6 +129,15 @@ public class SSLKeyManager implements X509KeyManager {
     
     public synchronized void removeKeyStore(String description) {
         _stores.remove(description);
+        _changeSupport.firePropertyChange(KEY_PROPERTY, null, null);
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        _changeSupport.addPropertyChangeListener(listener);
+    }
+    
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        _changeSupport.removePropertyChangeListener(listener);
     }
     
     public synchronized String[] getAliases(String description) {
@@ -113,31 +158,49 @@ public class SSLKeyManager implements X509KeyManager {
     }
     
     public synchronized boolean setPreferredAlias(String description, String alias) {
-        KeyStore ks = (KeyStore) _stores.get(description);
-        try {
-            if (ks.isKeyEntry(alias)) {
-                _preferredKeyManager = (X509KeyManager) _managers.get(description);
-                _preferredStore = description;
-                _preferredAlias = alias;
-                return true;
+        String old = String.valueOf(_preferredStore) + SEP + String.valueOf(_preferredAlias);
+        if (description != null && alias != null) {
+            KeyStore ks = (KeyStore) _stores.get(description);
+            try {
+                if (ks.isKeyEntry(alias)) {
+                    _preferredKeyManager = (X509KeyManager) _managers.get(description);
+                    _preferredStore = description;
+                    _preferredAlias = alias;
+                    String now = String.valueOf(_preferredStore) + SEP + String.valueOf(_preferredAlias);
+                    if (!now.equals(old)) 
+                        _changeSupport.firePropertyChange(SELECTED_KEY, null, null);
+                    return true;
+                }
+            } catch (KeyStoreException kse) {
+                _logger.severe("Unexpected KeyStore exception: " + kse.getMessage());
             }
-        } catch (KeyStoreException kse) {
-            _logger.severe("Unexpected KeyStore exception: " + kse.getMessage());
         }
         _preferredKeyManager = null;
         _preferredStore = null;
         _preferredAlias = null;
+        String now = String.valueOf(_preferredStore) + SEP + String.valueOf(_preferredAlias);
+        if (!now.equals(old)) 
+            _changeSupport.firePropertyChange(SELECTED_KEY, null, null);
         return false;
     }
     
+    public String getPreferredStore() {
+        return _preferredStore;
+    }
+    
+    public String getPreferredAlias() {
+        return _preferredAlias;
+    }
+    
     public synchronized String chooseClientAlias(String[] keyType, Principal[] issuers, Socket socket) {
+        _logger.entering(getClass().getName(), "chooseClientAlias");
         if (_preferredStore != null && _preferredAlias != null)
             return _preferredStore + SEP + _preferredAlias;
         return null;
     }
     
     public synchronized String chooseServerAlias(String keyType, Principal[] issuers, Socket socket) {
-        if (_preferredKeyManager != null) 
+        if (_preferredKeyManager != null)
             return _preferredKeyManager.chooseServerAlias(keyType, issuers, socket);
         
         Iterator it = _managers.keySet().iterator();
@@ -159,7 +222,7 @@ public class SSLKeyManager implements X509KeyManager {
     }
     
     public synchronized String[] getClientAliases(String keyType, Principal[] issuers) {
-        if (_preferredKeyManager != null) 
+        if (_preferredKeyManager != null)
             return _preferredKeyManager.getClientAliases(keyType, issuers);
         
         List allAliases = new ArrayList();
@@ -186,7 +249,7 @@ public class SSLKeyManager implements X509KeyManager {
     }
     
     public synchronized String[] getServerAliases(String keyType, Principal[] issuers) {
-        if (_preferredKeyManager != null) 
+        if (_preferredKeyManager != null)
             return _preferredKeyManager.getServerAliases(keyType, issuers);
         
         List allAliases = new ArrayList();
