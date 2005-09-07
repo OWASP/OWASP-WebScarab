05d29f2d39c0f6a19704dfcdb213fd660b4a916e
rogan <rogan>
2005-09-07 15:39:11 +0000

    improved error handling in split()

--- src/org/owasp/webscarab/model/NamedValue.java
+++ src/org/owasp/webscarab/model/NamedValue.java
@@ -6,6 +6,9 @@
 
 package org.owasp.webscarab.model;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 /**
  *
  * @author  rogan
@@ -14,6 +17,12 @@ public class NamedValue {
     
     private String _name;
     private String _value;
+    
+    private static Logger _logger = Logger.getLogger("org.owasp.webscarab.model.NamedValue");
+    
+    {
+        _logger.setLevel(Level.INFO);
+    }
     
     /** Creates a new instance of NamedValue */
     public NamedValue(String name, String value) {
@@ -34,14 +43,24 @@ public class NamedValue {
     }
     
     public static NamedValue[] splitNamedValues(String source, String pairSeparator, String nvSeparator) {
-        if (source == null) return new NamedValue[0];
-        String[] pairs = source.split(pairSeparator);
-        NamedValue[] values = new NamedValue[pairs.length];
-        for (int i=0; i<pairs.length; i++) {
-            String[] nv = pairs[i].split(nvSeparator,2);
-            values[i] = new NamedValue(nv[0], nv[1]);
+        try {
+            if (source == null) return new NamedValue[0];
+            String[] pairs = source.split(pairSeparator);
+            _logger.fine("Split \""+ source + "\" into " + pairs.length);
+            NamedValue[] values = new NamedValue[pairs.length];
+            for (int i=0; i<pairs.length; i++) {
+                String[] nv = pairs[i].split(nvSeparator,2);
+                if (nv.length == 2) { 
+                    values[i] = new NamedValue(nv[0], nv[1]);
+                } else {
+                    values[i] = null;
+                }
+            }
+            return values;
+        } catch (ArrayIndexOutOfBoundsException aioob) {
+            _logger.warning("Error splitting \"" + source + "\" using '" + pairSeparator + "' and '" + nvSeparator + "'");
         }
-        return values;
+        return new NamedValue[0];
     }
     
 }
