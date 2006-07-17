5b9339f63d3cc6963e864c54067ae6bc132d2e41
Rogan Dawes <rogan@dawes.za.net>
2006-07-17 18:19:46 +0200

    Change it from a JLabel to a JButton, so it is more interactive. Force an update when the gc has finished, so it shows the user that something happened

--- src/org/owasp/webscarab/util/swing/HeapMonitor.java
+++ src/org/owasp/webscarab/util/swing/HeapMonitor.java
@@ -6,17 +6,16 @@ package org.owasp.webscarab.util.swing;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
-import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 /**
  * @author rdawes
  * 
  */
-public class HeapMonitor extends JLabel implements ActionListener {
+public class HeapMonitor extends JButton implements ActionListener {
 
 	private long free;
 
@@ -35,13 +34,11 @@ public class HeapMonitor extends JLabel implements ActionListener {
 		timer = new Timer(15000, this);
 		timer.start();
 
-		addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				if (e.getClickCount() == 2) {
-					gc();
-				}
-			}
-		});
+                addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        gc();
+                    }
+                });
 	}
 
 	private void gc() {
@@ -49,6 +46,11 @@ public class HeapMonitor extends JLabel implements ActionListener {
 			public void run() {
 				System.out.println("Running gc");
 				System.gc();
+                                SwingUtilities.invokeLater(new Runnable() {
+                                    public void run() {
+                                        update();
+                                    }
+                                });
 			}
 		}.start();
 	}
