5e7829844640ef31a11ed1e2d58db4cbd8476370
Rogan Dawes <rdawes@athena.dawes.za.net>
2007-10-15 18:01:55 +0200

    Add a "hidden" interface for converting request from GET to POST
    Just right-click on the "Raw"/"Parsed" tab, and choose the
    option.

---
+++ src/org/owasp/webscarab/util/RequestConverter.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package org.owasp.webscarab.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.util.logging.Logger;
+
+import org.owasp.webscarab.model.HttpUrl;
+import org.owasp.webscarab.model.NamedValue;
+import org.owasp.webscarab.model.Request;
+
+/**
+ * @author rdawes
+ *
+ */
+public class RequestConverter {
+
+    private static Logger _logger = Logger.getLogger("org.owasp.webscarab.util.RequestConverter");
+    
+    public static Request convertGetToPost(Request get) {
+        if (!"GET".equals(get.getMethod()))
+            throw new IllegalArgumentException("Request must be a GET, not a " + get.getMethod());
+        Request post = new Request();
+        post.setMethod("POST");
+        HttpUrl url = get.getURL();
+        String query = url.getQuery();
+        if (query != null) {
+            try {
+                post.setContent(query.getBytes("ASCII"));
+            } catch (UnsupportedEncodingException uee) {
+                _logger.severe("Bizarre! " + uee.getLocalizedMessage());
+                throw new IllegalArgumentException("Unknown ASCII encoding!", uee);
+            }
+            String s = url.toString();
+            int q = s.indexOf('?');
+            s = s.substring(0, q);
+            try {
+                post.setURL(new HttpUrl(s));
+            } catch (MalformedURLException mue) {
+                throw new RuntimeException("Couldn't extract the POST url!", mue);
+            }
+        } else {
+            post.setURL(url);
+        }
+        post.setVersion(get.getVersion());
+        post.setHeaders(get.getHeaders());
+        post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        post.setHeader("Content-Length", Integer.toString(query == null ? 0 : query.length()));
+        return post;
+    }
+    
+    public static Request convertPostToMultipart(Request post) {
+        if (!"application/x-www-form-urlencoded".equals(post.getHeader("Content-Type")))
+            throw new IllegalArgumentException("Content type incorrect, was " + post.getHeader("Content-Type"));
+        StringBuffer buff = new StringBuffer();
+        Request multipart = new Request(post);
+        byte[] content = post.getContent();
+        if (content == null) 
+            content = new byte[0];
+        String sep = Encoding.hashMD5(content);
+        String contentType = "multipart/form-data; boundary=" + sep;
+        String boundary = "--" + sep;
+        String disposition = "Content-Disposition: form-data; name=";
+        NamedValue[] nvs = NamedValue.splitNamedValues(new String(content), "&", "=");
+        buff.append(boundary);
+        for (int i=0; i<nvs.length; i++) {
+            buff.append("\r\n").append(disposition).append("\"").append(nvs[i].getName()).append("\"\r\n\r\n");
+            buff.append(nvs[i].getValue()).append("\r\n").append(boundary);
+        }
+        buff.append("--\r\n");
+        multipart.setHeader("Content-Type", contentType);
+        multipart.setHeader("Content-Length", Integer.toString(buff.length()));
+        multipart.setContent(buff.toString().getBytes());
+        return multipart;
+    }
+    
+    public static Request convertGetToMultipartPost(Request request) {
+        return convertPostToMultipart(convertGetToPost(request));
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Request get = new Request();
+        get.setMethod("GET");
+        get.setURL(new HttpUrl("http://localhost/WebGoat/attack;fragment?a=1&b=nanana"));
+        get.setVersion("HTTP/1.0");
+        get.setHeader("Host", "localhost");
+        System.out.println(get +"\r\n=============\r\n");
+        Request post = convertGetToPost(get);
+        System.out.println(post + "\r\n==============\r\n");
+        Request multipart = convertPostToMultipart(post);
+        System.out.println(multipart + "\r\n================\r\n");
+        convertPostToMultipart(multipart);
+    }
+}
