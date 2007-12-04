3bf72ab667cb102657520a48581b87748ada88c7
Rogan Dawes <rogan@dawes.za.net>
2007-12-04 16:03:41 +0200

    BeanShell script editor: Auto indent when going to a new line

--- src/org/owasp/webscarab/plugin/proxy/swing/BeanShellPanel.java
+++ src/org/owasp/webscarab/plugin/proxy/swing/BeanShellPanel.java
@@ -26,7 +26,7 @@
  *
  * Source for this application is maintained at Sourceforge.net, a
  * repository for free software projects.
- * 
+ *
  * For details, please see http://www.sourceforge.net/projects/owasp
  *
  */
@@ -41,6 +41,14 @@
 package org.owasp.webscarab.plugin.proxy.swing;
 
 import bsh.EvalError;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
 import org.owasp.webscarab.plugin.proxy.BeanShell;
 import org.owasp.webscarab.plugin.proxy.BeanShellUI;
@@ -71,6 +79,7 @@ public class BeanShellPanel extends javax.swing.JPanel implements ProxyPluginUI,
         _beanShell = beanShell;
         
         initComponents();
+        scriptTextArea.registerKeyboardAction(new AutoIndentAction(), KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), JComponent.WHEN_FOCUSED);
         configure();
         
         _beanShell.setUI(this);
@@ -93,7 +102,7 @@ public class BeanShellPanel extends javax.swing.JPanel implements ProxyPluginUI,
         _docStream = new PrintStream(_dos);
         logTextArea.setDocument(_dos.getDocument());
     }
-
+    
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -171,9 +180,9 @@ public class BeanShellPanel extends javax.swing.JPanel implements ProxyPluginUI,
         add(jSplitPane1, java.awt.BorderLayout.CENTER);
 
     }//GEN-END:initComponents
-
+    
     private void commitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_commitButtonActionPerformed
-        try { 
+        try {
             _beanShell.setScript(scriptTextArea.getText());
         } catch (EvalError ee) {
             _logger.severe("Script error: " + ee);
@@ -186,10 +195,10 @@ public class BeanShellPanel extends javax.swing.JPanel implements ProxyPluginUI,
         scriptTextArea.setEnabled(enabled);
         commitButton.setEnabled(enabled);
     }//GEN-LAST:event_enableCheckBoxActionPerformed
-        
+    
     public JPanel getPanel() {
         return this;
-    }    
+    }
     
     public PrintStream getErr() {
         return _docStream;
@@ -217,5 +226,52 @@ public class BeanShellPanel extends javax.swing.JPanel implements ProxyPluginUI,
     private javax.swing.JPanel scriptPanel;
     private javax.swing.JTextArea scriptTextArea;
     // End of variables declaration//GEN-END:variables
+    
+    private static class AutoIndentAction extends AbstractAction {
+        public void actionPerformed(ActionEvent ae) {
+            JTextArea comp = (JTextArea)ae.getSource();
+            Document doc = comp.getDocument();
+            
+            if(!comp.isEditable())
+                return;
+            try {
+                int line = comp.getLineOfOffset(comp.getCaretPosition());
+                
+                int start = comp.getLineStartOffset(line);
+                int end = comp.getLineEndOffset(line);
+                String str = doc.getText(start, end - start - 1);
+                String whiteSpace = getLeadingWhiteSpace(str);
+                doc.insertString(comp.getCaretPosition(), '\n' + whiteSpace, null);
+            } catch(BadLocationException ex) {
+                try {
+                    doc.insertString(comp.getCaretPosition(), "\n", null);
+                } catch(BadLocationException ignore) {
+                    // ignore
+                }
+            }
+        }
+        
+        /**
+         *  Returns leading white space characters in the specified string.
+         */
+        private String getLeadingWhiteSpace(String str) {
+            return str.substring(0, getLeadingWhiteSpaceWidth(str));
+        }
+        
+        /**
+         *  Returns the number of leading white space characters in the specified string.
+         */
+        private int getLeadingWhiteSpaceWidth(String str) {
+            int whitespace = 0;
+            while(whitespace<str.length()) {
+                char ch = str.charAt(whitespace);
+                if(ch==' ' || ch=='\t')
+                    whitespace++;
+                else
+                    break;
+            }
+            return whitespace;
+        }
+    }
     
 }
