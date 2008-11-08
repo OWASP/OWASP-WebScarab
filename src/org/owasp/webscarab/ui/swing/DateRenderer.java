e0f79ed4e0f421a8dd3f9ccc87099c2020e0610b
Rogan Dawes <rogan@dawes.za.net>
2008-11-08 21:33:45 +0000

    DateRenderer: Make it automatically adjust to the available column width

--- src/org/owasp/webscarab/ui/swing/DateRenderer.java
+++ src/org/owasp/webscarab/ui/swing/DateRenderer.java
@@ -6,11 +6,13 @@
 
 package org.owasp.webscarab.ui.swing;
 
-import org.owasp.webscarab.model.Preferences;
-
 import java.util.Date;
+
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
+import java.awt.Component;
+import java.awt.FontMetrics;
 import java.text.SimpleDateFormat;
 
 /**
@@ -23,21 +25,34 @@ public class DateRenderer extends DefaultTableCellRenderer {
 	 * 
 	 */
 	private static final long serialVersionUID = 4146923038862167831L;
-	private SimpleDateFormat _sdf;
-    
-    public DateRenderer() {
-        String format = Preferences.getPreference("WebScarab.DateFormat", "yyyy/MM/dd HH:mm:ss");
-        _sdf = new SimpleDateFormat(format);
-    }
-    
-    public void setValue(Object value) {
-        if ((value != null) && (value instanceof Date)) {
-            Date date = (Date) value;
-            // value = DateUtil.rfc822Format(date);
-            value = _sdf.format(date);
-        }
-        super.setValue(value);
-    }
+	private static SimpleDateFormat[] formats = new SimpleDateFormat[] {
+		new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.S"),
+		new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"),
+		new SimpleDateFormat("MM/dd HH:mm:ss"),
+		new SimpleDateFormat("HH:mm:ss"), 
+		new SimpleDateFormat(":mm:ss")
+	};
+
+	public DateRenderer() {
+	}
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		if (value instanceof Date) {
+			Date date = (Date) value;
+			FontMetrics fm = getFontMetrics(getFont());
+			int i = 0;
+			String text;
+			int textWidth;
+			int targetWidth = table.getColumnModel().getColumn(column).getWidth() - 4;
+			do {
+				text = formats[i++].format(date); 
+				textWidth = fm.stringWidth( text );
+			} while (textWidth > targetWidth && i < formats.length);
+			setText(text);
+		}
+		return this;
+	}
     
 }
 
